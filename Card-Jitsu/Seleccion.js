139c2a4a9dce50877e210d495061ab702b6cf056
Larry Robles <134145081+Larryrzv@users.noreply.github.com>
2023-09-03 21:09:31 -0400

    Gran actualizacion
    Se agrego el nuevo modo de juego: "Modo PPT", de Piedras, Papel o Tijera.
    A su vez se agregaron y se modificaron los respectivos archivos para que esto.

--- Card-Jitsu/Seleccion.js
+++ Card-Jitsu/Seleccion.js
@@ -1,5 +1,8 @@
 // Constantes Globales
 const sectionSeleccionarAtaques = document.getElementById("seleccionar-ataques")
+const sectionSeleccionarElementoJugador = document.getElementById("Seleccionar-Elemento")
+const sectionSeleccionarModo = document.getElementById("seleccionarModo")
+
 const divReiniciar = document.getElementById("reiniciar")
 const botonReiniciar = document.getElementById("boton-reiniciar")
 const tutorial = document.getElementById("open")
@@ -13,8 +16,9 @@ const resultadoMensaje = document.getElementById("listaMensajes")
 const cajaEnfrentamiento = document.getElementById("enfrentamiento")
 const subtituloCombate = document.getElementById("subtitulo")
 
+const botonPPT = document.getElementById("modoPPT")
+const botonClassic = document.getElementById("modoClassic")
 
-const sectionSeleccionarElementoJugador = document.getElementById("Seleccionar-Elemento")
 const contenedorPersonajes = document.getElementById("contenedorPersonajes")
 const contenedorAtaques = document.getElementById ("CajaCartas")
 
@@ -25,6 +29,7 @@ let AGUA = "./resources/simboloDeAgua.png"
 let FUEGO = "./resources/simboloDeFuego.png"
 let NIEVE = "./resources/simboloDeNieve.png"
 let ninjasElementales = []
+let ninjasPPT = []
 let botonesElementales = []
 let isSelected = false
 let emojiAtaqueJugador = []
@@ -52,18 +57,17 @@ let indexResultado;
 
 // Clases
 class Ninjas{
-    constructor(nombre, foto, clase, vida,) {
+    constructor(nombre, foto, clase, score,) {
         this.nombre = nombre
         this.foto = foto
         this.clase = clase
-        this.vida = vida
+        this.score = score
         this.ataques = []
     }
 }
-let ninjaAgua = new Ninjas("NinjaAgua", "./resources/ninjaDeAgua.png", "tarjetaDeAgua", 3)
-let ninjaFuego = new Ninjas("NinjaFuego", "./resources/ninjaDeFuego.png", "tarjetaDeFuego", 3)
-let ninjaNieve = new Ninjas("NinjaNieve", "./resources/ninjaDeNieve.png", "tarjetaDeNieve", 3)
-
+let ninjaAgua = new Ninjas("NinjaAgua", "./resources/ninjaDeAgua.png", "tarjetaDeAgua", 0)
+let ninjaFuego = new Ninjas("NinjaFuego", "./resources/ninjaDeFuego.png", "tarjetaDeFuego", 0)
+let ninjaNieve = new Ninjas("NinjaNieve", "./resources/ninjaDeNieve.png", "tarjetaDeNieve", 0)
 ninjaAgua.ataques.push(
     { nombre: "Agua", id: "boton-Agua", img: "./resources/cartasDeAgua.png"},
     { nombre: "Agua", id: "boton-Agua", img: "./resources/cartasDeAgua.png"},
@@ -85,11 +89,260 @@ ninjaNieve.ataques.push(
     { nombre: "Agua", id: "boton-Agua", img: "./resources/cartasDeAgua.png"},
     { nombre: "Fuego", id: "boton-Fuego", img: "./resources/cartasDeFuego.png"},
 )
+class NinjasPPT{
+    constructor(nombre, foto, emoji, clase, vida,) {
+        this.nombre = nombre
+        this.foto = foto
+        this.emoji = emoji
+        this.clase = clase
+        this.vida = vida
+        this.ataques = []
+    }
+}
+let ninjaBlack1 = new NinjasPPT("Jugador", "./resources/ninjaComun1.png", "./resources/emojiSombra", "tarjetaJugador", 3)
+let ninjaBlack2 = new NinjasPPT("Rival", "./resources/ninja2.png", "./resources/emojiSombra", "tarjetaRival", 3)
+
+ninjaBlack1.ataques.push(
+    { nombre: "Nieve", id: "boton-Nieve", img: "./resources/cartasDeNieve.png"},
+    { nombre: "Agua", id: "boton-Agua", img: "./resources/cartasDeAgua.png"},
+    { nombre: "Fuego", id: "boton-Fuego", img: "./resources/cartasDeFuego.png"},
+)
+ninjaBlack2.ataques.push(
+    { nombre: "Nieve", id: "boton-Nieve", img: "./resources/cartasDeNieve.png"},
+    { nombre: "Agua", id: "boton-Agua", img: "./resources/cartasDeAgua.png"},
+    { nombre: "Fuego", id: "boton-Fuego", img: "./resources/cartasDeFuego.png"},
+)
 
 ninjasElementales.push(ninjaAgua,ninjaFuego,ninjaNieve)
+ninjasPPT.push(ninjaBlack1, ninjaBlack2)
 
 // Al Iniciar Juego
 function iniciarJuego() {
+    sectionSeleccionarAtaques.style.display = "none"
+    seleccionarElementoJugador
+    sectionSeleccionarElementoJugador.style.display = "none"
+
+    divReiniciar.style.display = "flex"
+
+
+    botonPPT.addEventListener("click", modoPPT)
+    botonClassic.addEventListener("click", modoClassic)
+    tutorial.addEventListener("click", popTutorial)
+
+
+}
+
+function modoPPT() {
+    sectionSeleccionarAtaques.style.display = "flex"
+    sectionSeleccionarModo.style.display = "none"
+
+    divReiniciar.style.display = "flex"
+    tutorial.style.display = "none"
+
+    openBotones.addEventListener("click", abrirBotonesElementales)
+    botonConfirmarCarta.addEventListener("click", encuentro)
+
+    botonReiniciar.addEventListener("click", reiniciarJuego)
+    divReiniciar.style.display = "flex"
+
+    edicionJugadorPPT()
+}
+//agregar la visualizacion del jugador para el modo PPT
+function edicionJugadorPPT() {
+let emojiElementoDelJugador
+let picJ;
+let configuracionDeCarta = document.getElementById("ContenidoTuyo")
+let contenidoTuyoImg = document.getElementById("contenidoTuyoimg")
+let colorBackground = document.getElementById("contenidoTuyo")
+
+        NinjaJugador = ninjaBlack1.nombre
+        emojiElementoDelJugador = ninjaBlack1.emoji
+        picJ = ninjaBlack1.foto
+        configuracionDeCarta.style.backgroundImage = "url(./resources/GemasDeFuegoNieveAgua.png)";
+        configuracionDeCarta.style.backgroundColor = "rgb(254 194 25)"
+        configuracionDeCarta.style.boxShadow = " 0px 0px 20px 2px rgb(255 144 58)"
+        configuracionDeCarta.style.marginBottom = "12px"
+        configuracionDeCarta.style.border = "12px solid rgb(243, 220, 13)"
+        contenidoTuyoImg.style.marginTop = "5px"
+        contenidoTuyoImg.style.width = "278px"
+        colorBackground.style.backgroundColor = "rgb(243, 220, 13)"
+    
+        document.getElementById("contenidoTuyoimg").src = picJ 
+        document.getElementById("emojiDeLaCardJugador").src = emojiElementoDelJugador
+
+    //Verificacion para los ataques
+if (!(emojiElementoDelJugador == "")) {
+    extraerAtaquesPPT(NinjaJugador)
+    edicionRivalPPT()
+    return (isSelected = true)
+}
+return (isSelected = false)
+}
+//agregar la visualizacion del Rival para el modo PPT
+function edicionRivalPPT() {
+let picR;
+let emojiElementoDelRival;
+let configuracionDeCartaRival = document.getElementById
+("ContenidoDelRival")
+let backgroundColorRival = document.getElementById("contenidoDelRival")
+let ContenidoRivalimg = document.getElementById("contenidoDelRivalimg")
+        NinjaJugador = ninjaAgua.nombre
+        emojiElementoDelRival = ninjaBlack2.emoji
+        picR = ninjaBlack2.foto
+        configuracionDeCartaRival.style.backgroundImage = "url(./resources/GemasDeFuegoNieveAgua.png)";
+        backgroundColorRival.style.backgroundColor = "rgb(243, 220, 13)"
+        configuracionDeCartaRival.style.backgroundColor = "rgb(254 194 25)"
+        configuracionDeCartaRival.style.boxShadow = "rgb(254 25 25) 0px 0px 20px 2px"
+        configuracionDeCartaRival.style.marginBottom = "21px"
+        configuracionDeCartaRival.style.border = "12px solid rgb(243, 220, 13)"
+        ContenidoRivalimg.style.marginTop = "5px"
+        ContenidoRivalimg.style.width = "278px"
+
+    document.getElementById("contenidoDelRivalimg").src = picR
+    document.getElementById("emojiDeLaCardRival").src = emojiElementoDelRival
+    AtaquesNinjaRival = ninjasPPT[ElementoAleatorio].ataques
+    subtituloCombate.style.width = "194px"
+}
+
+function extraerAtaquesPPT(NinjaJugador) {
+    let ataquesNinjasPPT
+    for (let i = 0; i < ninjasPPT.length; i++) {
+        if (NinjaJugador === ninjasPPT[i].nombre) {
+            ataquesNinjasPPT = ninjasPPT[i].ataques
+        }
+
+    }
+    mostrarAtaquesPPT(ataquesNinjasPPT)
+}
+function mostrarAtaquesPPT(ataquesNinjasPPT){
+    ataquesNinjasPPT.forEach((ataquesNinjasPPT)=> {
+        ataquesNinjaJugador = `
+        <button id=${ataquesNinjasPPT.id} class="boton-de-Ataque BAtaques" type="button">${ataquesNinjasPPT.nombre} <img src=${ataquesNinjasPPT.img} alt="medallon"></button>
+        `
+        contenedorAtaques.innerHTML += ataquesNinjaJugador
+    })
+    botones = document.querySelectorAll(".BAtaques")
+
+}
+
+
+function encuentro () {
+    contenedorAtaques.style.opacity = "0"
+    contenedorAtaques.style.pointerEvents = "none"
+    confirmarCarta.style.pointerEvents = "none"
+    confirmarCarta.style.opacity = "0"
+    ataquealeatorio = aleatorio(0, AtaquesNinjaRival.length -1)
+        if (ataquealeatorio == 2) {
+            emojiAtaqueRival = "./resources/simboloDeFuego.png"
+            AtaqueDelRival.push("FUEGO")
+            actualAtaqueRival = 2
+    
+        }   else if (ataquealeatorio == 3){ 
+                emojiAtaqueRival = "./resources/simboloDeAgua.png"
+                AtaqueDelRival.push("AGUA")
+                actualAtaqueRival = 3
+        }   else {
+            emojiAtaqueRival = "./resources/simboloDeNieve.png"
+            AtaqueDelRival.push("NIEVE")
+            actualAtaqueRival = 1
+        }
+                console.log(AtaqueDelRival)
+                resultadoPPT()
+                document.getElementById("ataqueElementalRival").src = emojiAtaqueRival
+    }
+
+
+function resultadoPPT() {
+    let innerEspada = document.getElementById("cajaMensajes")
+    botones.forEach((boton) => {
+        boton.addEventListener("click", (e) => {
+            if (e.target.textContent === "Agua ") {
+                ataquedelJugador.push("AGUA")
+                console.log(ataquedelJugador)
+                confirmarCarta.style.pointerEvents = "all"
+                confirmarCarta.style.opacity = "1"
+                seleccionJugador = 3
+                emojiAtaqueJugador = "./resources/simboloDeAgua.png"
+                emojiAtaqueRival = "./resources/cartas.png"
+                resultadoPPT = "⚔️"
+
+                innerEspada.innerHTML = resultadoPPT
+                document.getElementById("ataqueElementalJugador").src = emojiAtaqueJugador
+                document.getElementById("ataqueElementalRival").src = emojiAtaqueRival
+            }
+            else if (e.target.textContent === "Fuego ") {
+                ataquedelJugador.push("FUEGO")
+                console.log(ataquedelJugador)
+                confirmarCarta.style.pointerEvents = "all"
+                confirmarCarta.style.opacity = "1"
+                seleccionJugador = 2
+                emojiAtaqueJugador = "./resources/simboloDeFuego.png"
+                emojiAtaqueRival = "./resources/cartas.png"
+                resultadoPPT = "⚔️"
+                innerEspada.innerHTML = resultadoPPT
+                document.getElementById("ataqueElementalJugador").src = emojiAtaqueJugador
+                document.getElementById("ataqueElementalRival").src = emojiAtaqueRival
+            }
+            else {
+                ataquedelJugador.push("NIEVE")
+                console.log(ataquedelJugador)
+                confirmarCarta.style.pointerEvents = "all"
+                confirmarCarta.style.opacity = "1"
+                seleccionJugador = 1
+                emojiAtaqueJugador = "./resources/simboloDeNieve.png"
+                emojiAtaqueRival = "./resources/cartas.png"
+                resultadoPPT = "⚔️"
+
+                innerEspada.innerHTML = resultadoPPT
+                document.getElementById("ataqueElementalJugador").src = emojiAtaqueJugador
+                document.getElementById("ataqueElementalRival").src = emojiAtaqueRival
+            }
+            revisarVidas ()
+            crearMensajesPPT ()
+        })
+    })
+}
+
+function revisarVidas (){
+    let configuracionDeCarta = document.getElementById("ContenidoTuyo")
+    let configuracionDeCartaRival = document.getElementById("ContenidoDelRival")
+    let cajaMensajes = document.getElementById("mensajes2")
+    if(vidasJugador == 0){
+        crearMesajeFinal("Lo Siento, Has Perdido, vuelve a intentarlo")
+        desabilitarBotonesCombates()
+        cajaMensajes.style.background = "#7f4d5a"
+        cajaMensajes.style.fontSize = "11mm"
+        cajaMensajes.style.boxShadow = "rgb(147 62 84) 0px 0px 20px 5px"
+        configuracionDeCarta.style.opacity = "0"
+    }
+    else if (vidasRival == 0) {
+        crearMesajeFinal("¡¡Felicidades Has Ganado!! Quieres seguir con tu racha? Vuelve a intentarlo")
+        desabilitarBotonesCombates()
+        cajaMensajes.style.background = "#FFEB3B"
+        cajaMensajes.style.boxShadow = "rgb(255 232 53) 0px 0px 20px 5px"
+        configuracionDeCartaRival.style.opacity = "0"}
+}function crearMensajesPPT() {
+    let resultadoMensaje = document.getElementById("cajaMensajes")
+    let divemojiAtaqueJugador = document.getElementById("ataqueElementalJugador")
+    let divemojiAtaqueRival = document.getElementById("ataqueElementalRival")
+
+        resultadoMensaje.innerHTML =  resultadoPPT
+        divemojiAtaqueJugador.innerHTML = emojiAtaqueJugador
+        divemojiAtaqueRival.innerHTML = emojiAtaqueRival
+}
+
+function crearMesajeFinal(resultadoFinal) {
+    let sectionResultadoFinal = document.getElementById("mensajes2")
+    let parrafo2 = document.createElement("p")
+    parrafo2.innerHTML = resultadoFinal
+    
+    sectionResultadoFinal.appendChild(parrafo2)
+}
+
+
+
+
+function modoClassic() {
     sectionSeleccionarAtaques.style.display = "none"
 
     sectionVerMapa.style.display = "none"
@@ -104,15 +357,13 @@ function iniciarJuego() {
 
     })
 
-    divReiniciar.style.display = "none"
+    divReiniciar.style.display = "flex"
 
     openBotones.addEventListener("click", abrirBotonesElementales)
     botonConfirmarCarta.addEventListener("click", resultados)
 
     botonReiniciar.addEventListener("click", reiniciarJuego)
     
-    tutorial.addEventListener("click", popTutorial)
-
     inputcheckedAgua = document.getElementById("NinjaAgua")
     inputcheckedNieve = document.getElementById("NinjaNieve")
     inputcheckedFuego = document.getElementById("NinjaFuego")
@@ -149,7 +400,6 @@ function popTutorial() {
     popupDelTutorial.style.opacity = "0"
     popupDelTutorial.style.pointerEvents = "none"
 }
-
 
 // Seleccion Elemento del Jugador
 function seleccionarElementoJugador() {
@@ -215,7 +465,6 @@ if (!(emojiElementoDelJugador == "")) {
 }
 return (isSelected = false)
 }
-
 function extraerAtaques(NinjaJugador) {
     let ataquesNinja
     for (let i = 0; i < ninjasElementales.length; i++) {
@@ -224,10 +473,9 @@ function extraerAtaques(NinjaJugador) {
         }
         
     }
-    mostrarAtaques(ataquesNinja)
+    mostrarAtaquesClassic(ataquesNinja)
 }
-
-function mostrarAtaques(ataquesNinja){
+function mostrarAtaquesClassic(ataquesNinja){
     ataquesNinja.forEach((ataquesNinja)=> {
         ataquesNinjaJugador = `
         <button id=${ataquesNinja.id} class="boton-de-Ataque BAtaques" type="button">${ataquesNinja.nombre} <img src=${ataquesNinja.img} alt="medallon"></button>
@@ -312,6 +560,7 @@ function abrirBotonesElementales() {
         contenedorAtaques.style.pointerEvents = "all"
     }
 }
+
 
 function secuenciaAtaque() {
     botones.forEach((boton) => {
